f0f47c54ddc61a07adbe62abbfd27580639f0b38
jtb <jan@boee.org>
2024-06-17 09:22:44 +0200

    Update start_tracer function parameters
    The 'start_tracer' function was modified to accept URL as a variable input instead of using a static URL. This will make the 'start_tracer' function more versatile in different environments.

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,9 @@ use crate::opentelclient::logs_service_client::LogsServiceClient;
 
 mod opentelclient;
 
-pub async fn start_tracer() {
+pub async fn start_tracer(url: &str) {
     set_global_default(GrpcSubscriber::new(LogsServiceClient::new(
-        Channel::from_static("http://0.0.0.0:4317")
+        Channel::from_static(url)
             .connect()
             .await
             .unwrap(),
